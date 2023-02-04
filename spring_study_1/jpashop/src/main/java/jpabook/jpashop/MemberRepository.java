8dc54a3d8db053505dfe367238fde826f0e4376f
BH946 <kobh2895@gmail.com>
2023-02-04 21:57:47 +0900

    test : h2 db 연동 테스트

---
+++ spring_study_1/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    EntityManager em; // 위 어노테이션을 통해 자동 생성
+
+    public Long save(Member member) {
+        em.persist(member); // db 저장
+        return member.getId();
+    }
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
