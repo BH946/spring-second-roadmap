11cbbfe40fcf4321c9cb447c7dc9ca89b64af266
BH946 <kobh2895@gmail.com>
2023-02-07 17:01:47 +0900

    add : 주문엔티티(비지니스, 생성, 조회 로직), 주문상품 엔티티 마찬가지 수정, 주문레퍼지토리개발

--- spring_study_1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ spring_study_1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -39,7 +39,7 @@ public class Order {
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
 
 
-    // 연관관계 메서드 => 코드를 줄여줌
+    //==연관관계 메서드==// => 코드를 줄여줌
     public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this); // orders 리스트에 추가
@@ -53,6 +53,45 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==// => 수많은 정보들 저장하는걸 한번에 이 메서드에서 해결
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+    
+    //==비지니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if(delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+    
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 
 }
