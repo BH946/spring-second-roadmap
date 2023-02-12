61826cba27b386876d87150a5d399650704039e9
BH946 <kobh2895@gmail.com>
2023-02-12 23:56:59 +0900

    add : 회원 수정, 회원 조회 API 개발

--- spring_study_2/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ spring_study_2/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,14 +3,15 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController // @Controller + @RequestBody
 @RequiredArgsConstructor // 생성자 + @Autowired
@@ -62,4 +63,76 @@ public class MemberApiController {
         @NotEmpty // 이렇게 쓸 수 있다는것도 큰 장점
         private String name;
     }
+
+    /**
+     * 수정 API
+     * > 오류정정: 회원수정 API updateMemberV2 은 회원 정보를 부분 업데이트한다. 여기서 PUT 방식을
+     * 사용했는데, PUT은 전체 업데이트를 할때 사용하는 것이 맞다. 부분 업데이트를 하려면 PATCH를
+     * 사용하거나 POST를 사용하는것이 REST 스타일에 맞다.
+     */
+//    @PutMapping("/api/v2/members/{id}") // 수정은 PUT 사용
+    @PostMapping("/api/v2/members/{id}") // 수정은 PUT 사용
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName()); // update함수 새로 개발(변경감지를사용해서 데이터를수정)
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+
+    /**
+     * 조회 V1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+     * 문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     *   - 기본적으로 엔티티의 모든 값이 노출된다.
+     *   - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JsonIgnore, 별도의 뷰 로직 등등)
+     *   - 실무에서는 같은 엔티티에 대해 API가 용도에 따라 다양하게 만들어지는데, 한 엔티티에 각각의
+     API를 위한 프레젠테이션 응답 로직을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * - 추가로 컬렉션을 직접 반환하면 항후 API 스펙을 변경하기 어렵다.(별도의 Result 클래스
+     생성으로 해결)
+     * 결론
+     * - API 응답 스펙에 맞추어 별도의 DTO를 반환한다.
+     */
+    //조회 V1: 안 좋은 버전, 모든 엔티티가 노출, @JsonIgnore -> 이건 정말 최악, api가 이거 하나인가! 화면에 종속적이지 마라!
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    /**
+     * 조회 V2: 응답 값으로 엔티티가 아닌 별도의 DTO를 반환한다.
+     * 또한 어레이가 아닌 오브젝트에 어레이를 담아서 json 반환!
+     */
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        //엔티티 -> DTO 변환
+        List<MemberDto> collect = findMembers.stream() // stream으로 풀기
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
 }
