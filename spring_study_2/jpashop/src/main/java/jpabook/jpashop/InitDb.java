7d0e7285d4835dd80cdadcca2c6258d6fddd3166
BH946 <kobh2895@gmail.com>
2023-03-04 21:51:10 +0900

    add : api 테스트용 데이터 셋

---
+++ spring_study_2/jpashop/src/main/java/jpabook/jpashop/InitDb.java
@@ -0,0 +1,71 @@
+package jpabook.jpashop;
+
+
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.item.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+
+/**
+ * 앱실행시 총 주문 2개 자동 insert
+ * userA
+     * JPA1 BOOK
+     * JPA2 BOOK
+ * userB
+     * SPRING1 BOOK
+     * SPRING2 BOOK
+ */
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+    private final InitService initService;
+
+    @PostConstruct // 앱 실행 시점에 해당 메소드 실행(참고 : @PreDestroy 는 종료시점)
+    public void init() {
+        initService.dbInit1();
+        initService.dbInit2();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final EntityManager em;
+
+        public void dbInit1() {
+            // Ctrl + Alt + M 으로 간단히 메소드들 리팩토링 가능
+            Member member = new Member();
+            member.setName("userA");
+            member.setAddress(new Address("서울", "1", "1111"));
+            em.persist(member);
+
+            Book book1 = new Book();
+            book1.setName("JPA1 BOOK");
+            book1.setPrice(10000);
+            book1.setStockQuantity(100);
+            em.persist(book1);
+
+            Book book2 = new Book();
+            book2.setName("JPA2 BOOK");
+            book2.setPrice(20000);
+            book2.setStockQuantity(100);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+    }
+
+}
