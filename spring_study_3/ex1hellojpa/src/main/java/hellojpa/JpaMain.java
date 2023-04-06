5acb2c4b16247421e2fb29aff188772df88b1030
BH946 <kobh2895@gmail.com>
2023-04-07 00:16:11 +0900

    add : 1차 캐시 테스트

--- spring_study_3/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ spring_study_3/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -20,9 +20,13 @@ public class JpaMain {
             Member member = new Member();
             member.setId(2L);
             member.setName("HelloB");
+            // 1차 캐시에 저장. 즉, 영속성 컨텍스트에 저장
             em.persist(member);
+            // 1차 캐시에서 조회
+            Member findMember = em.find(Member.class, 2L);
+            System.out.println("findMember.id = " + findMember.getId());
 
-            tx.commit();
+            tx.commit(); // 이때 쿼리 날라감. (insert 쿼리만 날라감. select 없고)
         }catch (Exception e) {
             tx.rollback();
         }finally {
