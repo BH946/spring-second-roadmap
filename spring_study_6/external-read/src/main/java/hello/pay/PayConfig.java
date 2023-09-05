a441a713d5c20f5599ef71a064d81a928a29aa10
BH946 <kobh2895@gmail.com>
2023-09-05 18:06:45 +0900

    add : @Profile 사용해서 등록한 프로필에 따라 빈에 등록

--- spring_study_6/external-read/src/main/java/hello/pay/PayConfig.java
+++ spring_study_6/external-read/src/main/java/hello/pay/PayConfig.java
@@ -1,2 +1,27 @@
-package hello.pay;public class PayConfig {
+package hello.pay;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * @Profile 에 파라미터로 등록한 프로필에 따라 "빈에 등록"
+ */
+@Slf4j
+@Configuration
+public class PayConfig {
+    @Bean
+    @Profile("default")
+    public LocalPayClient localPayClient() {
+        log.info("LocalPayClient 빈 등록");
+        return new LocalPayClient();
+    }
+
+    @Bean
+    @Profile("prod")
+    public ProdPayClient prodPayClient() {
+        log.info("ProdPayClient 빈 등록");
+        return new ProdPayClient();
+    }
 }
