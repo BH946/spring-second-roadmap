3cac5e98281f649a89b0c04f4cd40339299d4cb1
BH946 <kobh2895@gmail.com>
2023-09-05 18:09:22 +0900

    add : 테스트 위해 세팅 - 컴포넌트 스캔 범위 추가 및 앱 실행 로직 추가

--- spring_study_6/external-read/src/main/java/hello/ExternalReadApplication.java
+++ spring_study_6/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Import;
 
 
 @Import(MyDataSourceConfigV3.class) // 해당 파일 컴포넌트 스캔
-@SpringBootApplication(scanBasePackages = "hello.datasource") // 컴포넌트 스캔 범위
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"}) // 컴포넌트 스캔 범위
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
