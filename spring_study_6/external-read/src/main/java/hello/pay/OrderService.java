62db18ca8d1fd1342c301c1ecad52b0e65ba3ddb
BH946 <kobh2895@gmail.com>
2023-09-05 18:05:18 +0900

    add : 페이 계산하는 서비스 로직 구현

--- spring_study_6/external-read/src/main/java/hello/pay/OrderService.java
+++ spring_study_6/external-read/src/main/java/hello/pay/OrderService.java
@@ -1,2 +1,18 @@
-package hello.pay;public class OrderService {
+package hello.pay;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * 프로필에 따라 LocalPayClient 또는 ProdPayClient 주입받게 될 것
+ */
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final PayClient payClient; // LocalPayClient or ProdPayClient
+
+    public void order(int money) {
+        payClient.pay(money);
+    }
 }
