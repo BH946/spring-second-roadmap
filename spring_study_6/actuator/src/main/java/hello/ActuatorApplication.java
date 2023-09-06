d37a172952c4719aa84d118b2807820af24720dd
BH946 <kobh2895@gmail.com>
2023-09-06 20:30:23 +0900

    add : 게이지 테스트 완료

--- spring_study_6/actuator/src/main/java/hello/ActuatorApplication.java
+++ spring_study_6/actuator/src/main/java/hello/ActuatorApplication.java
@@ -2,6 +2,7 @@ package hello;
 
 import hello.order.OrderConfigV2;
 import hello.order.OrderConfigV4;
+import hello.order.StockConfigV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Import;
 
 
 //@Import(OrderConfigV2.class)
-@Import(OrderConfigV4.class)
+//@Import(OrderConfigV4.class)
+@Import({OrderConfigV4.class, StockConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
